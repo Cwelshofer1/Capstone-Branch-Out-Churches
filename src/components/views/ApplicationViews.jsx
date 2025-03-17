8f37d30809b3d5ec9500f8540fde48a8a7a7fbf7
Cwelshofer1 <sweetbrooo@gmail.com>
2025-03-17 14:28:23 -0500

    working auth

---
+++ src/components/views/ApplicationViews.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+
+export const ApplicationViews = () => {
+
+    return <>
+    <Routes>
+        <Route path="/" element={
+            <>
+            
+            </>
+        }></Route>
+
+    </Routes>
+    </>
+}
---
+++ src/views/ApplicationViews.jsx
@@ -0,0 +1,3 @@
+export const ApplicationViews = () => {
+  return <><h1>Hello world!!</h1></>
+}
